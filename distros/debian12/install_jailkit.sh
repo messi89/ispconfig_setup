522fff8404d8e0705ba1d485cc8473cabeb577b2
josefjebavy <email@josefjebavy.cz>
2024-04-05 20:12:50 +0200

    we dont need other packages

--- distros/debian12/install_jailkit.sh
+++ distros/debian12/install_jailkit.sh
@@ -3,13 +3,10 @@
 #    Install Jailkit
 #---------------------------------------------------------------------
 
-#Program Versions
-JKV="2.23"  #Jailkit Version -> Maybe this can be automated
-SUM="c7018645430248613c6241bf529d95ef"
 
 InstallJailkit() {
   echo -n "Installing Jailkit... "
-  apt_install build-essential autoconf automake libtool flex bison debhelper binutils jailkit
+  apt_install jailkit
   echo -e "[${green}DONE${NC}]\n"
 }
 
