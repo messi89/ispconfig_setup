e8dd21572eda301b52437e750c98d310ed2b0286
josefjebavy <email@josefjebavy.cz>
2024-04-05 21:07:40 +0200

    fix set root password

--- distros/debian12/install_mysql.sh
+++ distros/debian12/install_mysql.sh
@@ -9,7 +9,7 @@ InstallSQLServer() {
     apt_install mariadb-client mariadb-server
     sed -i 's/bind-address		= 127.0.0.1/#bind-address		= 127.0.0.1\nsql-mode="NO_ENGINE_SUBSTITUTION"/' /etc/mysql/mariadb.conf.d/50-server.cnf
 	sed -i 's/password =/password = '$CFG_MYSQL_ROOT_PWD'/' /etc/mysql/debian.cnf
-	mysql -e "UPDATE mysql.user SET Password = PASSWORD('$CFG_MYSQL_ROOT_PWD') WHERE User = 'root'"
+	mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '$CFG_MYSQL_ROOT_PWD';"
 	# Make our changes take effect
 	mysql -e "FLUSH PRIVILEGES"
 	echo -e "[${green}DONE${NC}]\n"
