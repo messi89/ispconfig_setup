9e860a1d478afa2b7ef6f9eeb711a849039c99c0
josefjebavy <email@josefjebavy.cz>
2024-04-05 21:29:46 +0200

    remove bad and old commands

--- distros/debian12/install_mysql.sh
+++ distros/debian12/install_mysql.sh
@@ -4,14 +4,9 @@
 #---------------------------------------------------------------------
 InstallSQLServer() {
     echo -n "Installing Database server (MariaDB)... "
-    echo "maria-db-10.1 mysql-server/root_password password $CFG_MYSQL_ROOT_PWD" | debconf-set-selections
-    echo "maria-db-10.1 mysql-server/root_password_again password $CFG_MYSQL_ROOT_PWD" | debconf-set-selections
     apt_install mariadb-client mariadb-server
     sed -i 's/bind-address		= 127.0.0.1/#bind-address		= 127.0.0.1\nsql-mode="NO_ENGINE_SUBSTITUTION"/' /etc/mysql/mariadb.conf.d/50-server.cnf
-	sed -i 's/password =/password = '$CFG_MYSQL_ROOT_PWD'/' /etc/mysql/debian.cnf
-	mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '$CFG_MYSQL_ROOT_PWD';"
-	# Make our changes take effect
-	mysql -e "FLUSH PRIVILEGES"
+
 	echo -e "[${green}DONE${NC}]\n"
 	echo -n "Restarting MariaDB... "
 	systemctl restart mysql
